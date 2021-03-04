ec3cbc517c3fa22c863a1c6a58022cfc649ebbd4
Yoshifumi Tsuji <JPZ3177@w022581907C48m.local>
2021-03-04 18:44:02 +0900

    Part5 finished

---
+++ src/components/InboxScreen.js
@@ -0,0 +1,42 @@
+// src/components/InboxScreen.js
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import TaskList from './TaskList';
+
+export function PureInboxScreen({error}) {
+    if(error) {
+        return (
+            <div className="page lists-show">
+                <div className="wrapper-message">
+                    <span className="icon-face-sad" />
+                    <div className="title-message">Oh no!</div>
+                    <div className="subtitle-message">Something went wrong</div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="page lists-show">
+            <nav>
+                <h1 className="title-page">
+                    <sapn className="title-wrapper">Taskbox</sapn>
+                </h1>
+            </nav>
+            <TaskList />
+        </div>
+    );
+}
+
+PureInboxScreen.propTypes = {
+    error: PropTypes.string,
+}
+
+PureInboxScreen.defaultProps = {
+    error: null,
+}
+
+export default connect(({ error }) => ({ error }))(PureInboxScreen);
